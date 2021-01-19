72461dcce48a81f12dd5d3428723dd4c99fcec82
Brian Murphy <blpmurphy@gmail.com>
2021-01-19 20:55:25 +0000

    Update checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -22,25 +22,25 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
 
         # Set autofocus attribute on the full name field
         self.fields['full_name'].widget.attrs['autofocus'] = True
         # iterate through the forms fields
         for field in self.fields:
-            if self.fields[field].required:
-                # adding a star if field required
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            # setting all placeholders to their values in above dictionary
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    # adding a star if field required
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                # setting all placeholders to their values in above dictionary
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             # Add a CSS class called striped-style-input
             self.fields[field].widget.attrs['class'] = 'striped-style-input'
             # Removing the form fields labels
