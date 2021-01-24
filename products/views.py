7c5c7db10bacba462b0050bd3ab083c3b1cb6b16
Brian Murphy <blpmurphy@gmail.com>
2021-01-24 12:29:14 +0000

    Add Delete products view for admin use

--- products/views.py
+++ products/views.py
@@ -76,15 +76,14 @@ def product_detail(request, product_id):
 
 
 def add_product(request):
-
     """ Add a product to the store """
     if request.method == 'POST':
         # request.FILES handles the img files being uploaded
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(
                 request,
@@ -117,10 +116,18 @@ def edit_product(request, product_id):
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
 
-    template = 'products/add_product.html'
+    template = 'products/edit_product.html'
     context = {
         'form': form,
         'product': product,
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product has been deleted!')
+    return redirect(reverse('products'))
